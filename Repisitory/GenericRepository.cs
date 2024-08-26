3130a9284410f0a074bc4e42854be6e7ac5ba731
danieljobe351 <76815935+danieljobe351@users.noreply.github.com>
2024-08-26 16:54:30 +0200

    Add files via upload

---
+++ Repisitory/GenericRepository.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using TelemetryPortal_MVC.Data;
+using TelemetryPortal_MVC.Repisitory;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+        protected readonly TechtrendsContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(TechtrendsContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+  
+        public void Add(T entity)
+        {
+
+            _context.Set<T>().Add(entity);
+
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+
+        }
+    
+
+
+public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+       public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync();
+        }
+       
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id) != null;
+        }
+    }
+
+
+
+
---
+++ Repisitory/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using TelemetryPortal_MVC.Models;
+namespace TelemetryPortal_MVC.Repisitory
+{
+    public interface IProjectRepository : IGenericRepository<Project>
+    {
+        Project GetProject();
+        Task<Project> GetProjectByIdAsync(Guid? id);
+        Task CreateProjectAsync(Project client);
+        Task UpdateProjectAsync(Project client);
+        Task DeleteProjectAsync(Guid id);
+        bool ProjectExists(Guid id);
+        
+    }
+}
